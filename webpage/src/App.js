261401a63a4ece7e56682f28b0e3aa4d8588fd40
david fletcher <davidfletcheraaron@gmail.com>
2021-11-23 21:37:20 -0800

    showing Home

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <p> Hello World !</p>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Home />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
