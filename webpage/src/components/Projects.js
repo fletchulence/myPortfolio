8e64808472dc556aea1591b6d7037031c0dc0c52
david fletcher <davidfletcheraaron@gmail.com>
2021-11-30 08:51:36 -0800

    projects component  started

---
+++ webpage/src/components/Projects.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+//Links to projects
+const Link1 = 'https://nasa-photo-of-the-day-seven-kappa.vercel.app/'; // nasa PhotoOfTheDay
+// const Link2 = 
+
+const projectsArr = [
+   { Link1 },
+   // { Link2 },
+   // { Link3 }
+]
+
+function Projects() {
+   return (
+      <div>
+         -- these are my projects -- <br/> there will be a grid of projects here
+      </div>
+   )
+}
+
+export default Projects
