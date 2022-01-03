559f563cc6153ae92356d17ef1c9c0dd3b952a57
david fletcher <davidfletcheraaron@gmail.com>
2022-01-03 15:18:33 -0500

    michelle example

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -9,7 +9,7 @@ import Projects from './components/Projects';
 function App() {
   return (
     <div className="App">
-      <p> Hello Justin !</p>
+      <p> Hello Michelle !</p>
       <Header />
 
       <Routes>
