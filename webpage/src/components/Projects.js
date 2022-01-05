2991eb452b4b6b2ce18a0bb585c35d31b01b9cfd
david fletcher <davidfletcheraaron@gmail.com>
2022-01-04 23:23:41 -0500

    projects are a WIP for now

--- webpage/src/components/Projects.js
+++ webpage/src/components/Projects.js
@@ -1,16 +1,18 @@
 //TODO:  STILL NEED TO INSTALL MUI ?
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-//Links to projects
-// const Link1 = 'https://nasa-photo-of-the-day-seven-kappa.vercel.app/'; // nasa PhotoOfTheDay
-// const Link2 = 'https://front-end-tau-ashen.vercel.app/'
+// Links to projects
+const Link1 = 'https://nasa-photo-of-the-day-seven-kappa.vercel.app/'; // nasa PhotoOfTheDay
+const Link2 = 'https://front-end-tau-ashen.vercel.app/'
 
-let ProjLinks = [
-   'https://nasa-photo-of-the-day-seven-kappa.vercel.app/',
-   'https://front-end-tau-ashen.vercel.app/',
-   'google.com'
-
+const ProjLinks = [
+   // 'https://nasa-photo-of-the-day-seven-kappa.vercel.app/',
+   // 'https://front-end-tau-ashen.vercel.app/',
+   <Link to={Link2}> hello </Link>,
+   <Link to={Link1}> Nasa Photo of the Day </Link>,
+   // {Link2}
 ]
 
 // let arr = [];
@@ -25,13 +27,14 @@ function Projects(props) {
    // console.log(arr)
    return (
       <div>
+         <h1> Projects </h1>
          <p>
             -- these are my projects -- <br/> 
             there will be a grid of projects here:
          </p>
          {ProjLinks.map((e)=>   
             <ul> 
-               <li> {e}</li> 
+               <li> {e} </li> 
             </ul> 
          )}
       </div>
