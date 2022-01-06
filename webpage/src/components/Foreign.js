5b64802e0bb74d4062761bf35f7bcd95ffe5f144
david fletcher <davidfletcheraaron@gmail.com>
2022-01-06 18:33:06 -0500

    played around with a tutorial found in docs

--- webpage/src/components/Foreign.js
+++ webpage/src/components/Foreign.js
@@ -11,11 +11,18 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+// import ExitToAppIcon from '@mui/icons-material/Home';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+// import potluck_thumb from '../assets/images/potluck_home.png';
+// import anywhere_thumb from '../assets/images/anywhere_dashboard1.png';
+// import nasa_thumb from '../assets/images/nasa_home.png';
+
+
 const ExpandMore = styled((props) => {
+   console.log(props)
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
@@ -26,48 +33,51 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function RecipeReviewCard() {
+export default function ProjectOverviewCard(props) {
+   const { name, role, linkFor, bullet1, bullet2, bullet3 } = props;
+   console.log(linkFor, bullet2, props.image);
   const [expanded, setExpanded] = React.useState(false);
+  const [ liked, setLiked ] = React.useState(false)
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
+  const handleLinkClick = () =>{
+     console.log('show me', linkFor)
+  }
+
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
+          <Avatar sx={{ bgcolor: red[400] }} aria-label="recipe"> PP </Avatar>
         }
         action={
           <IconButton aria-label="settings">
-            <MoreVertIcon />
+            <MoreVertIcon /* //todo:make a modal to open for viewing more  */ />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={name}
+        subheader={`Role: ${role}`}
       />
       <CardMedia
         component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
+        height="200"
+        image={props.image}
+        alt="Potluck Project"
       />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
+      {/* <CardContent>
+        <Typography variant="body2" color="text.secondary"></Typography>
+        <Typography variant="body2"> as;dklfj </Typography>
+      </CardContent> */}
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
         <IconButton aria-label="share">
-          <ShareIcon />
+          <ExitToAppIcon onClick={handleLinkClick} />
         </IconButton>
         <ExpandMore
           expand={expanded}
@@ -82,29 +92,17 @@ export default function RecipeReviewCard() {
         <CardContent>
           <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+             {bullet1}
           </Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            {bullet2}
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
+            {bullet3}
           </Typography>
-          <Typography>
+          {/* <Typography>
             Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          </Typography> */}
         </CardContent>
       </Collapse>
     </Card>
