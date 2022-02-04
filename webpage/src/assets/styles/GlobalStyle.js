4580c3db08187fb71a7e3432ccc4aa63ea584309
david fletcher <davidfletcheraaron@gmail.com>
2022-02-04 18:37:53 -0500

    switching to a header branch

--- webpage/src/assets/styles/GlobalStyle.js
+++ webpage/src/assets/styles/GlobalStyle.js
@@ -3,12 +3,14 @@ import theme from "./themes";
 
 const GlobalStyle = createGlobalStyle`
    body{
-      // background-color: ${theme.palette.primary.light};
+      background-color: ${theme.palette.primary.light};
       color: ${theme.palette.tertiary.text};
+      // overflow:visible;
+
    }
    #vanta{
       width: 100%;
-      height: 100%;
+      height: 100vh;
       // z-index: -1;
    }
 
