ad7d268c12abc098330383de3fcbbbe9dfb3cef3
david fletcher <davidfletcheraaron@gmail.com>
2022-03-25 22:11:56 -0400

    login is using state

--- webpage/src/components/secretComponents/Login.js
+++ webpage/src/components/secretComponents/Login.js
@@ -1,20 +1,53 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Login(props) {
+   const navigate = useNavigate();
+   // console.log(props)
+   const [state, setState] = useState({
+      fullname: '',
+      password:''
+   });
 
-   // onchange 
-   // onSubmit
+   function handleChange (evt) {
+      setState({
+         ...state,
+         [evt.target.name]: evt.target.value
+      });
+   };
+   
+   function handleSubmit(evt) {
+      evt.preventDefault();
+      if (state.fullname === 'debra' && state.password === 'fletcher') {
+         console.log('this will be correct for you')
+         navigate('/fishCal')
+      } else {
+         alert('Seems you entered your Name and Password wrong! Try again, please')
+      }
+   };
    // route to the protected
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
          <label>
             Name: 
-            <input type='text' name='fullname'/> 
+            <input 
+               type='text' 
+               name='fullname' 
+               placeholder="please input name"
+               onChange={handleChange}
+               value={state.fullname}
+               /> 
          </label>
          <label>
             Password: 
-            <input type='text' name='password'/> 
+            <input 
+               placeholder="please input password"
+               type='text' 
+               name='password' 
+               onChange={handleChange}
+               value={state.password}
+            /> 
          </label>
          <input type="submit" value='Submit'/>
       </form>
