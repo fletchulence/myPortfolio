cf6b45b85fc9b76501ad40d11d8191f2200f1202
david fletcher <davidfletcheraaron@gmail.com>
2022-03-25 22:12:31 -0400

    organization ont he cal

--- webpage/src/components/FishCal.js
+++ webpage/src/components/FishCal.js
@@ -1,16 +1,11 @@
-import React from "react";
-import Calendar from './Calendar'
-
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+
+import Calendar from './Calendar'
 
 import LoginForm from "./secretComponents/Login";
 
-// function LoggedIn() {
-//    return false;
-// };
-
-const FishCal = () => {
+const FishCal = (props) => {
    console.log('im working')
    // let navigate = useNavigate();
    let LoggedIn = false;
