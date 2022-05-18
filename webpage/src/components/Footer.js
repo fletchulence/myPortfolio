9bff5e5818d9cda92d4e4d6d322bb4fe9cf861b3
david fletcher <davidfletcheraaron@gmail.com>
2022-05-18 13:30:44 -0400

    github logo added - not connect

--- webpage/src/components/Footer.js
+++ webpage/src/components/Footer.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from './../assets/icons/githublogo.png'
 
-
+const GitLogo = styled.img`
+  background-image: url(${logo});
+  width: 20%;
+  height: 10vh;
+  background-size: contain;
+  background-repeat: no-repeat;
+`
 
 export default function Footer() {
   return (
-    <>
-    Footer
-    </>
+    <div>
+      <GitLogo/>
+    <nav>
+      <a href='https://www.linkedin.com/in/david-a-fletcher/'> LinkedIn </a>
+      <a href='https://github.com/fletchulence'>github</a>
+      {/* <GitLogo todo: add an oncilck so that it will go to my page/> */}
+
+      Facebook? No
+      View resume -&gt; protected
+    </nav>
+    </div>
   )
 }
