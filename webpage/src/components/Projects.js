1253c5a43e25f1e44145d59619336a372648bc2e
david fletcher <davidfletcheraaron@gmail.com>
2022-05-19 10:51:40 -0400

    changed images and added AMP

--- webpage/src/components/Projects.js
+++ webpage/src/components/Projects.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 // image imports
-import anywhere_thumb from '../assets/images/anywhere_dashboard1.png';
-import potluck_thumb from '../assets/images/potluck_home.png';
-import nasa_thumb from '../assets/images/nasa_home.png';
+import anywhere_thumb from '../assets/images/anywhere_dashboard1.webp';
+import potluck_thumb from '../assets/images/potluck_home.webp';
+import nasa_thumb from '../assets/images/nasa_home.webp';
+import african_mp from '../assets/images/African_marketPlace.webp';
 
 // importing theme -- when can i stop doing this??
 import theme from './../assets/styles/themes'
@@ -11,7 +12,7 @@ import theme from './../assets/styles/themes'
 import ProjCard from './ProjectCard'
 
 // MUI IMPORTS:
-import { Card, Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 //todo: add alt txt for the imgs
 const ProjCardInfo = [
@@ -24,6 +25,8 @@ const ProjCardInfo = [
       bullet2: 'I was the only person assigned to the back end of this project. I am still working out kinks.',
       bullet3: 'Trying to add the classes to each person, so that they auto populate correctly. I will probably be working on this project for some time :)',
       icon_color: '#ef5350',
+      github_link: 'https://github.com/Build-Week-Anywhere-Fitness-6-2021',
+      comments_section: ''
    },
    {
       proj_name: 'Potluck Planner',
@@ -34,6 +37,8 @@ const ProjCardInfo = [
       bullet2: 'fixed issues with styling',
       bullet3: 'Passed props where appropriate',
       icon_color: '#ef5350',
+      github_link: 'https://github.com/Potluck-Planner-A/front-end',
+      comments_section: ''
    },
    {
       proj_name: 'Nasa Photo of the Day',
@@ -44,30 +49,34 @@ const ProjCardInfo = [
       bullet2: 'Learned to make a calendar output with a form. This was very advanced for the time i started doing it, but i started understanding more and more as time went on.',
       bullet3: 'You will notice that nothing in this project is strictly written. All is populated. Fully modular.',
       icon_color: theme.palette.primary.borders,
+      github_link: 'https://github.com/fletchulence/nasa-photo-of-the-day',
+      comments_section: ''
    },
    {
-      proj_name: 'Nasa Photo of the Day',
+      proj_name: 'African Marketplace',
       role: 'Creator',
-      image: nasa_thumb,
-      linkFor: 'https://nasa-photo-of-the-day-seven-kappa.vercel.app/',
-      bullet1: 'went above and beyond on this project -- doesnt look too complex, but this was the first time that i actually worked with pulling from an API. I am very happy with this product.',
-      bullet2: 'Learned to make a calendar output with a form. This was very advanced for the time i started doing it, but i started understanding more and more as time went on.',
-      bullet3: 'You will notice that nothing in this project is strictly written. All is populated. Fully modular.',
+      image: african_mp,
+      linkFor: '',
+      bullet1: 'This was my First experience working on a team and with github',
+      bullet2: 'Used mostly styled components and theme setting to make the frontend for the web page',
+      bullet3: 'The API isnt pulling correctly from the one that was set up, but this was something that happened beacuse we didnt have a proper devoted team member in the backend role. Eventually I want to go back to this and redevelop a backend for it.',
       icon_color: theme.palette.tertiary.text,
+      github_link: 'https://github.com/Build-Week-ft-african-marketplace-1/front-end/tree/main/african-marketplace',
+      comments_section: ''
    },
 ]
 
 function Projects() {
    return (
-      <Card
-         variant="outlined"
+      <Container
          sx={{
             display: 'flex',
             flexFlow: 'row wrap',
             justifyContent: 'center',
             outline: theme.palette.primary.main.text,
-            backgroundColor: theme.palette.primary.light,
+            // backgroundColor: 'clear',
          }} >
+            {/* <h1> Projects: </h1> */}
          <Box sx={{
             display: 'flex',
             flexFlow: 'row wrap',
@@ -77,19 +86,19 @@ function Projects() {
          }}>
             {ProjCardInfo.map((e, idx) =>
                <ProjCard
-                  key={idx}
-                  proj_name={e.proj_name}
-                  role={e.role}
-                  linkFor={e.linkFor}
-                  image={e.image}
-                  bullet1={e.bullet1}
-                  bullet2={e.bullet2}
-                  bullet3={e.bullet3}
-                  icon_color={e.icon_color}
+               key={idx}
+               proj_name={e.proj_name}
+               role={e.role}
+               linkFor={e.linkFor}
+               image={e.image}
+               bullet1={e.bullet1}
+               bullet2={e.bullet2}
+               bullet3={e.bullet3}
+               github_link={e.github_link}
                > {e} </ProjCard>
-            )}
+               )}
          </Box>
-      </Card>
+      </Container>
    )
 }
 
