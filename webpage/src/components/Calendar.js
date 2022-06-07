8516c082b5f849d8e6e1840fda72907711a323bd
david fletcher <davidfletcheraaron@gmail.com>
2022-06-07 15:33:05 -0400

    Calendar picker set up

--- webpage/src/components/Calendar.js
+++ webpage/src/components/Calendar.js
@@ -1,42 +1,76 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import CalendarPicker from '@mui/lab/CalendarPicker';
-import MonthPicker from '@mui/lab/MonthPicker';
-import YearPicker from '@mui/lab/YearPicker';
-import Grid from '@mui/material/Grid';
+import { 
+  LocalizationProvider,
+  CalendarPicker,
+  MonthPicker,
+  DatePicker,
+  YearPicker,
+} from '@mui/lab';
+import { 
+  TextField, 
+  Grid,
+  Badge,
+  Container,
+  Paper,
+  Card,
+} from '@mui/material';
 
 const minDate = new Date('2020-01-01T00:00:00.000');
 const maxDate = new Date('2034-01-01T00:00:00.000');
 
 export default function SubComponentsPickers() {
-  const [date, setDate] = React.useState(new Date());
-  console.log('this is hitting')
+  const [date, setDate] = useState(new Date());
+
+  const getRandomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+  }
+
+  const handleDateChange = (newDateValue) => {
+    setDate(newDateValue)
+  }
+
+  // const handleMonthChange = async () => {
+  //   // just select random days to simulate server side based data
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       setSelectedDays([1, 2, 3].map(() => getRandomNumber(1, 28)));
+  //       resolve();
+  //     }, 1000);
+  //   });
+  // };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <CalendarPicker date={date} onChange={(newDate) => setDate(newDate)} />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <MonthPicker
-            date={date}
-            minDate={minDate}
-            maxDate={maxDate}
-            onChange={(newDate) => setDate(newDate)}
+      <Grid container justifyContent="space-around">
+        <DatePicker
+          disableToolbar
+          margin='normal'
+          label='this calendar'
+          variant='inline'
+          value={date}
+          onChange={handleDateChange}
+          renderInput={(props) => <TextField {...props} margin='normal' label='Date' helperText='valid mask'/> }
           />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <YearPicker
-            date={date}
-            isDateDisabled={() => false}
-            minDate={minDate}
-            maxDate={maxDate}
-            onChange={(newDate) => setDate(newDate)}
-          />
-        </Grid>
       </Grid>
+      <Grid container xs={12} md={6}>
+        <CalendarPicker 
+          label='this calendar'
+          value={date}
+          onChange={handleDateChange}
+          renderInput={(props) => <TextField {...props} margin='normal' label='Date' helperText='valid mask'/> }
+        />
+      </Grid>
+      <Container>im here
+        <Grid container spacing={0}>
+          <Grid item>
+            <Paper>a;lskdjf</Paper>
+          </Grid>
+          <Paper>
+            helloooo
+          </Paper>
+        </Grid>
+      </Container>
     </LocalizationProvider>
   );
 }
