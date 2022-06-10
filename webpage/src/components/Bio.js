8ea37086dcae96d7d1513c95e11de6420cc7b303
david fletcher <davidfletcheraaron@gmail.com>
2022-06-10 01:11:34 -0400

    changed background and made it dim Background

--- webpage/src/components/Bio.js
+++ webpage/src/components/Bio.js
@@ -4,17 +4,28 @@ import React from "react";
 import {
    Container,
    Box,
+   Typography
 } from '@mui/material';
 
 export default function Bio() {
    return (
       <Container id='Bio' maxWidth='xl'>
-         <Box sx={{ display: 'flex', color: "red", backgroundColor: "black" }}>
-            <ul>
-               <li> worked on a farm </li>
-               <li> degree in MechE </li>
-               <li> looking to be a fullStack Dev for the REST OF TIME</li>
-            </ul>
+         <Box sx={{
+            boxShadow: '10',
+            border:'1px solid white', 
+            borderRadius:'1rem', 
+            display: 'flex', 
+            backgroundColor: "#0000004D", 
+            padding:'2rem',
+            }}>
+            <Typography variant='h4' sx={{backgroundBlendMode:'difference', opacity:'1.2', fontWeight:'medium', color: 'dark'}}>
+               why i left sales
+                  <Typography paragraph> Talk about sales </Typography>
+               why i studied mechanical engineering
+                  <Typography paragraph> Talk about sales </Typography>
+               why I learned how to be a developer
+                  <Typography paragraph> Talk about sales </Typography>
+            </Typography>
          </Box>
       </Container>
    )
